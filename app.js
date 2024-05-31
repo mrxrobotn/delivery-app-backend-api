a0da050bc65cd93f1e4c1d3a7430e3a5a6b932af
Mouez B'k <benkhalil.mouez@gmail.com>
2024-05-31 20:24:18 +0100

    update

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import multer from 'multer';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import fs from 'fs';
 import File from './src/models/audio.js'
 
@@ -32,43 +33,46 @@ mongoose
     console.log(err);
   });
 
+// Get __dirname equivalent
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-   // Multer setup
-   const storage = multer.diskStorage({
-     destination: (req, file, cb) => {
-       const uploadPath = path.join(__dirname, 'uploads');
-       if (!fs.existsSync(uploadPath)) {
-         fs.mkdirSync(uploadPath);
-       }
-       cb(null, uploadPath);
-     },
-     filename: (req, file, cb) => {
-       cb(null, `${Date.now()}-${file.originalname}`);
-     },
-   });
+// Multer setup
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const uploadPath = path.join(__dirname, 'uploads');
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath);
+    }
+    cb(null, uploadPath);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
 
-   const upload = multer({ storage });
+const upload = multer({ storage });
 
-    // Endpoint to handle file uploads
-    app.post(`${apiURL}/upload`, upload.single('file'), async (req, res) => {
-      try {
-        const fileData = new File({
-          filename: req.file.filename,
-          path: req.file.path,
-          originalName: req.file.originalname,
-        });
- 
-        await fileData.save();
- 
-        res.status(200).send({
-          message: 'File uploaded successfully',
-          fileUrl: `${apiURL}/uploads/${req.file.filename}`,
-        });
-      } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-      }
+// Endpoint to handle file uploads
+app.post(`${apiURL}/upload`, upload.single('file'), async (req, res) => {
+  try {
+    const fileData = new File({
+      filename: req.file.filename,
+      path: req.file.path,
+      originalName: req.file.originalname,
     });
+
+    await fileData.save();
+
+    res.status(200).send({
+      message: 'File uploaded successfully',
+      fileUrl: `${apiURL}/uploads/${req.file.filename}`,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
