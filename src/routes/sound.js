74d7969d9fee41b9031d46479cda6c93b6bc2cdb
Mouez B'k <benkhalil.mouez@gmail.com>
2024-05-31 21:41:31 +0100

    update

--- src/routes/sound.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { uploadMiddleware, uploadFile } from '../controllers/sound.js';
-
-const router = express.Router();
-
-router.route("/upload").post(uploadMiddleware, uploadFile);
-
-export default router;
